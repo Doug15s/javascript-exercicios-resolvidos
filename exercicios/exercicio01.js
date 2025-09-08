19a26de0d7c53b7378bb0b7343902ae4cc36b7f7
Douglas de Barros <douglasbl18@gmail.com>
2025-09-08 11:52:51 -0300

    Adicionada resolução do exercício 01 e inserção da linha no arquivo README.md

--- exercicios/exercicio01.js
+++ exercicios/exercicio01.js
@@ -1,4 +1,12 @@
 /*
 01 - Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor
-que C
-/*
+que C.
+*/
+
+let A = prompt("Digite o valor de A:");
+let B = prompt("Digite o valor de B:");
+let C = prompt("Digite o valor de C:");
+
+if (A + B < C) {
+    
+}
